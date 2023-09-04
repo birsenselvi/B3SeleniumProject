91f9c05096579779cd1435eb5bd838d7a11c25c3
atezgider <atezgider@gmail.com>
2023-09-04 22:01:31 +0200

    04.09

---
+++ src/main/java/gun26_testng/TestNGReportListener.java
@@ -0,0 +1,38 @@
+package gun26_testng;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestNGReportListener {
+    /*
+        Menü -> Run -> Edit Configuration
+        Sol taraftan ilgili Test classi secilir
+        Sag tarafta Listener Tabi secilir
+        Alttaki + ya tiklanir
+        Acilan kutuda "reporter" yazilir
+        Listelenen eklentilerden "eMailable reporter" secilir
+        Apply -> Ok
+
+        Sonra ilgili Test Class'i run edilir
+
+        project root'da "test-output" klasörü altinda "emailable-report.html" olusturulur
+
+     */
+
+    @Test
+    public void test1(){
+
+        Assert.assertEquals(1, 1);
+
+    }
+
+    @Test
+    public void test2(){
+        Assert.assertEquals(1, 2);
+    }
+
+    @Test
+    public void test3(){
+        Assert.assertEquals(2, 2);
+    }
+}
