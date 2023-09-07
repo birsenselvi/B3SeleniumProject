7513fb7a21965d8c701a03da7b0b349ee1833bc8
atezgider <atezgider@gmail.com>
2023-09-07 19:52:19 +0200

    07.09

---
+++ src/main/java/gun29/MyElements.java
@@ -0,0 +1,37 @@
+package gun29;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import utils.Driver;
+
+import java.time.Duration;
+
+public class MyElements {
+
+    public static MyElements $(By locator){
+        return new MyElements(locator);
+    }
+
+    private WebDriver driver;
+    private WebDriverWait wait;
+    private By locator;
+
+    private MyElements(By locator){
+        this.locator = locator;
+        driver = Driver.getDriver();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+
+    public MyElements sendKey(String text){
+        wait.until(ExpectedConditions.elementToBeClickable(locator)).sendKeys(text);
+        return this;
+    }
+
+    public MyElements click(){
+        wait.until(ExpectedConditions.elementToBeClickable(locator)).click();
+        return this;
+    }
+
+}
