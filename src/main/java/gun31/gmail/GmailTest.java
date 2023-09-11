35219bcb0ef0a6800ed5cf286f633eb9d01cb010
atezgider <atezgider@gmail.com>
2023-09-11 20:21:08 +0200

    11.09

---
+++ src/main/java/gun31/gmail/GmailTest.java
@@ -0,0 +1,49 @@
+package gun31.gmail;
+
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class GmailTest {
+
+    String url = "https://mail.google.com";
+    WebDriver driver;
+    WebDriverWait wait;
+
+    By lEmail = By.cssSelector("input[type='email']");
+    By lContinue = By.xpath("//button[contains(., 'Weiter')]");
+    String eMail = "atezgider@gmail.com";
+
+    @Before
+    public void beforeClass(){
+        configure();
+    }
+
+    @Test
+    public void testGMail(){
+        driver.get(url);
+        //wait.until(ExpectedConditions.visibilityOfElementLocated(lEmail)).sendKeys(eMail);
+        //driver.findElement(lContinue).click();
+
+    }
+
+    public void configure(){
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--start-maximized");
+        options.addArguments("--headless");
+        options.addArguments("user-data-dir=C:\\Users\\pc\\AppData\\Local\\Google\\Chrome\\User Data");
+        driver = new ChromeDriver(options);
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+
+
+}
